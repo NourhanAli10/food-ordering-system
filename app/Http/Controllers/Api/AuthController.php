6d15a22a0af2a0c1cd2bed46e171b77587b0b348
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2026-01-18 00:23:40 +0200

    Add user authentication APIs: register, login, and logout

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Traits\ApiResponsesTrait;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    use ApiResponsesTrait;
+
+    public function register(Request $request)
+    {
+
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'phone' => 'required|string|max:12|unique:users,phone',
+            'password' => 'required|string|min:8',
+        ]);
+
+        $user = User::create($validated);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return $this->successResponse(
+            message: 'user has been registered successfully',
+            data: [
+                'user' => $user,
+                'token_type' => 'Bearer',
+                'access_token' => $token,
+            ],
+            statusCode: 201
+        );
+    }
+
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email|string',
+            'password' => 'required|string',
+        ]);
+
+
+        if (! Auth::attempt($request->only('email', 'password'))) {
+            return $this->errorResponse(message: ' The provided credentials are incorrect.', statusCode: 401);
+        }
+
+        $user = Auth::user();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return $this->successResponse(
+            message: 'you have login successfully',
+            data: [
+                'user' => $user,
+                'token_type' => "Bearer",
+                'access_token' => $token
+            ],
+        );
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return $this->successResponse(message: 'you have logout successfully');
+    }
+}
---
+++ app/Traits/ApiResponsesTrait.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Traits;
+
+trait ApiResponsesTrait
+{
+
+    public function successResponse(string $message, array $data = [], int $statusCode = 200)
+    {
+        return response()->json([
+            'status' => 'success',
+            'message' => $message ?? "success",
+            'data' => $data,
+            'errors' => [],
+        ], $statusCode);
+    }
+
+
+    public function errorResponse(string $message, array $errors = [], int $statusCode = 404)
+    {
+        return response()->json([
+            'status' => 'failed',
+            'message' => $message ?? "failed",
+            'data' => [],
+            'errors' => $errors ?? [],
+        ], $statusCode);
+    }
+}
